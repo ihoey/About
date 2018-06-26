5f4d96e37a7c93efa912b6f6001bf9b7aaf6625c
ihoey <mail@ihoey.com>
2018-06-26 21:39:57 +0800

    mod

--- cron.sh
+++ cron.sh
@@ -3,4 +3,5 @@ echo "_______" >> temp.txt
 git add .
 git commit -m"mod"
 git push origin master
-
+git push coding master
+curl https://sc.ftqq.com/SCU10625Td571049c53dd2e36148f1343a18544ef59855df9df77c.send?text=about更新完成~
