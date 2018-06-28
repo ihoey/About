c4115f99bc59faa3c6d2b488bdf7891bf62f54f8
ihoey <mail@ihoey.com>
2018-06-28 15:12:18 +0800

    date

--- cron.sh
+++ cron.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
+
 cd /data/www/resume/
+
 echo "_______" >> temp.txt
+
 git add --all
-git commit -m"mod"
+
+git commit -m date
+
 git push origin master
+
 git push coding master
+
 curl https://sc.ftqq.com/SCU10625Td571049c53dd2e36148f1343a18544ef59855df9df77c.send?text=about更新完成~
