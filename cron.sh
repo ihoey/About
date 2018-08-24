d21207b5974852420116959273dbb00c8d83c880
ihoey <mail@ihoey.com>
2018-08-24 10:50:04 +0800

    2018年 08月 24日 星期五 10:50:04 CST

--- cron.sh
+++ cron.sh
@@ -4,7 +4,7 @@ DATE=`date`
 
 cd /data/www/resume/
 
-echo "_______" >> temp.txt
+echo DATE >> text.log
 
 git add --all
 
@@ -14,4 +14,4 @@ git push origin master
 
 git push coding master
 
-curl https://sc.ftqq.com/SCU10625Td571049c53dd2e36148f1343a18544ef59855df9df77c.send?text=about更新完成~
+curl https://sc.ftqq.com/SCU10625Td571049c53dd2e36148f1343a18544ef59855df9df77c.send?text="$DATE"
