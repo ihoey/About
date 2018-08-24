034b32dcda0b791beaac2aa1d7514379789757d9
ihoey <mail@ihoey.com>
2018-08-24 11:18:30 +0800

    为毛我的眼里常含泪水，因为我老是躺着中枪。 hitokoto

--- cron.sh
+++ cron.sh
@@ -15,4 +15,4 @@ git push origin master
 
 git push coding master
 
-curl https://sc.ftqq.com/SCU10625Td571049c53dd2e36148f1343a18544ef59855df9df77c.send?text=更新成功&desp="$TEXT"
+curl https://sc.ftqq.com/SCU10625Td571049c53dd2e36148f1343a18544ef59855df9df77c.send?text=hitokoto&desp="$TEXT"
