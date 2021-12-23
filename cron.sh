9b8a9f1283f05f52c8d4b7217d0430dff422c985
ihoey <mail@ihoey.com>
2021-12-23 11:23:39 +0800

    feat: change ftqq key

--- cron.sh
+++ cron.sh
@@ -19,6 +19,6 @@ git push origin master
 
 git push coding master
 
-curl https://sc.ftqq.com/SCU10625Td571049c53dd2e36148f1343a18544ef59855df9df77c.send?text="$TEXT"
+curl https://sc.ftqq.com/SCT106175TfTMJN6BtvfkOSiGkMaGKigV6.send?text="$TEXT"
 
 # node ./index.js
