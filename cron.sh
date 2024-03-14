536f616b213ccae2e829b5f159adc2c38898a855
ihoey <mail@ihoey.com>
2024-03-14 11:03:08 +0800

    "就像那句狠话：有一天，我一定会后悔的。"

--- cron.sh
+++ cron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DATE=`date`
-TEXT=$(curl https://v1.hitokoto.cn | python -c "import sys, json; reload(sys); sys.setdefaultencoding('utf8'); print json.load(sys.stdin)['hitokoto']")
+TEXT=$(curl https://v1.hitokoto.cn | jq ".hitokoto")
 
 cd /data/www/resume/
 
